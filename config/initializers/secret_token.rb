2f5e1ce65b2f1fafb7997fdc7374b77b6ae7f400
pepato <rpepato@gmail.com>
2011-11-17 21:30:30 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talkthetalk::Application.config.secret_token = 'dd41180b0ff95a422e5d4466f8e5249b49d16ee9dd039ca18542565a94baf04d2c7ad021deae43a49cb8cfafe76b42d360d558f25e056bdc6de18c2b90498a6f'
