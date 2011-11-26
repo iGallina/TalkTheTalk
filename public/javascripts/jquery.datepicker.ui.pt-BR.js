fa60c7a90c05e88a8eff73ee3c14399c96d1c368
Rafael Lisboa <rafaelzlisboa@gmail.com>
2011-11-26 07:13:36 -0200

    Some cleanup, translate a few strings to pt-BR and improve event edit form design

--- public/javascripts/jquery.datepicker.ui.pt-BR.js
+++ public/javascripts/jquery.datepicker.ui.pt-BR.js
@@ -3,22 +3,24 @@
 jQuery(function($){
         $.datepicker.regional['pt-BR'] = {
                 closeText: 'Fechar',
-                prevText: '&#x3c;Anterior',
-                nextText: 'Pr&oacute;ximo&#x3e;',
+                prevText: '&#x3c; Anterior',
+                nextText: 'Próximo &#x3e;',
                 currentText: 'Hoje',
-                monthNames: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho',
-                'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-                monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun',
-                'Jul','Ago','Set','Out','Nov','Dez'],
-                dayNames: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
-                dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
-                dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+                monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+                	'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+                monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
+                	'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+                dayNames: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
+                dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+                dayNamesMin: ['D','S','T','Q','Q','S','S'],
                 weekHeader: 'Sm',
                 dateFormat: 'dd/mm/yy',
                 firstDay: 0,
                 isRTL: false,
                 showMonthAfterYear: false,
-                yearSuffix: ''};
+                yearSuffix: ''
+	};
+
         $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
 });
 
